f05adad56a1ac9f303895eb49943be4657e21bdd
RyanVerhey <ryan90v@gmail.com>
2014-06-18 15:33:03 -0500

    Added PayrollController#run method that makes the script run

--- payroll_calc.rb
+++ payroll_calc.rb
@@ -7,6 +7,20 @@ class PayrollController
     @payday = nil
   end
 
+  def run
+    puts "Welcome to Ryan's Payroll Calculator!"
+    puts ""
+    get_start_date
+    puts @start_date
+    puts ""
+    get_pay_interval
+    puts @pay_interval
+    puts ""
+    get_payday
+    puts @payday
+    puts ""
+    # print_list_of_dates(PayrollCalculator.calculate(@start_date, @pay_interval, @payday))
+  end
 end
 
 class PayrollCalculator
@@ -15,3 +29,4 @@ class PayrollCalculator
 end
 
 controller = PayrollController.new
+controller.run
