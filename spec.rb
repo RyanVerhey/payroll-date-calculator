9fd5efeb4e83fb0878c5f927f0a2d38759f0fb05
RyanVerhey <ryan90v@gmail.com>
2014-06-18 15:24:35 -0500

    Specs for the PayrollController#run method

--- spec.rb
+++ spec.rb
@@ -2,8 +2,18 @@ require 'rspec'
 require_relative 'payroll_calc'
 
 describe PayrollController do
+  let(:controller) { PayrollController.new }
+
   it "should be a class" do
     expect(PayrollController.new).to be_an_instance_of(PayrollController)
+  end
+
+  context '#run' do
+    it 'should call #get_start_date, get_pay_interval' do
+      expect(controller).to receive(:get_start_date)
+      expect(controller).to receive(:get_pay_interval)
+      controller.run
+    end
   end
 end
 
