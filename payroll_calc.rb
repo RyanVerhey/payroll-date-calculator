dd8b29f0bdc03c698939258bcb2cf9e1ed185416
RyanVerhey <ryan90v@gmail.com>
2014-06-18 15:34:13 -0500

    Added PayrollController#get_start_date and #invalid_date_reset
    If the date is invalid, #invalid_date_reset is run which tells the user there was something wrong and runs #get_start_date to get the start date from the user again

--- payroll_calc.rb
+++ payroll_calc.rb
@@ -21,6 +21,32 @@ class PayrollController
     puts ""
     # print_list_of_dates(PayrollCalculator.calculate(@start_date, @pay_interval, @payday))
   end
+
+  def get_start_date(input_date = nil)
+    puts "Is there a specific start date you want to start from? Please use the MM/DD/YYYY format."
+    puts "If not, just press Enter and the start date will be today."
+    input_date ||= gets.chomp!
+    return_date = nil
+    if input_date =~ /\A\d{2}\/\d{2}\/\d{4}\z/
+      begin
+        @start_date = Date.strptime(input_date, '%m/%d/%Y')
+      rescue
+        invalid_date_reset
+      end
+    elsif input_date == ""
+      @start_date = Date.today
+    else
+      invalid_date_reset
+    end
+    @start_date
+  end
+
+  def invalid_date_reset
+    puts ""
+    puts "I'm sorry, that is not a valid date. Please try again."
+    puts ""
+    get_start_date
+  end
 end
 
 class PayrollCalculator
