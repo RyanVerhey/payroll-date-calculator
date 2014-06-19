4802a3fd8b75f490d9f3999caee3aed657c15f73
RyanVerhey <ryan90v@gmail.com>
2014-06-19 17:28:45 -0500

    Moved Date class extension to own file

---
+++ date_extension.rb
@@ -0,0 +1,21 @@
+class Date
+  def this_week
+    self - self.wday
+  end
+
+  def next_week
+    self + (7 - self.wday)
+  end
+
+  def next_wday(n)
+    n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
+  end
+
+  def last_week
+    self.this_week - 7
+  end
+
+  def last_wday(n)
+    n < self.wday ? self - (self.wday - n) : self.last_week.next_day(n)
+  end
+end
--- payroll_calc.rb
+++ payroll_calc.rb
@@ -1,34 +1,11 @@
 require 'date'
+require_relative 'date_extension'
 
 WEEK_DAYS = { "monday" => 1,
               "tuesday" => 2,
               "wednesday" => 3,
               "thursday" => 4,
               "friday" => 5 }
-
-class Date
-  def this_week
-    self - self.wday
-  end
-
-  def next_week
-    self + (7 - self.wday)
-  end
-
-  def next_wday(n)
-    n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
-  end
-
-  def last_week
-    self.this_week - 7
-  end
-
-  def last_wday(n)
-    n < self.wday ? self - (self.wday - n) : self.last_week.next_day(n)
-  end
-end
-
-
 
 class PayrollController
   def initialize(start_date = nil, pay_interval = nil, payday = nil, holiday_filename = nil)
