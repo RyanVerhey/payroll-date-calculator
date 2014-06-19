14469ac39825e87a5e934ff00b367c5610d64869
RyanVerhey <ryan90v@gmail.com>
2014-06-19 12:25:14 -0500

    Edited tests for new @holidays variable

--- spec.rb
+++ spec.rb
@@ -128,14 +128,14 @@ describe PayrollCalculator do
     end
     it 'should move to the next day if pay interval is daily and the day is a holiday' do
       start_date = Date.strptime('06/23/2014', '%m/%d/%Y')
-      PayrollCalculator.class_variable_set(:@@holidays, [Date.strptime('06/24/2014', '%m/%d/%Y')])
+      PayrollCalculator.holidays = [Date.strptime('06/24/2014', '%m/%d/%Y')]
       date_arr = PayrollCalculator.calculate(start_date, "daily", start_date.wday, 1)
       expect(date_arr[0]).to eq('06/23/2014')
       expect(date_arr[1]).to eq('06/25/2014')
     end
     it 'should return to regular payday schedule if payday lands on a holiday or weekend' do
       start_date = Date.strptime('06/23/2014', '%m/%d/%Y')
-      PayrollCalculator.class_variable_set(:@@holidays, [Date.strptime('06/30/2014', '%m/%d/%Y')])
+      PayrollCalculator.holidays = [Date.strptime('06/30/2014', '%m/%d/%Y')]
       date_arr = PayrollCalculator.calculate(start_date, "weekly", start_date.wday, 2)
       expect(date_arr[0]).to eq('06/23/2014')
       expect(date_arr[1]).to eq('06/27/2014')
