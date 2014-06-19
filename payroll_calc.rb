1211a93db2840fc2ce831f3cd4ab1e9f28f4e0ea
RyanVerhey <ryan90v@gmail.com>
2014-06-18 22:11:36 -0500

    Extended Date class
    I extended the date class with a few methods to make my life easier: A method that finds the start of a week, one that finds the start of the next week, one that finds the next certain weekday, one that finds the start of the previous week, and one that finds a certain weekday in the previous week

--- payroll_calc.rb
+++ payroll_calc.rb
@@ -6,6 +6,28 @@ WEEK_DAYS = { "monday" => 1,
               "thursday" => 4,
               "friday" => 5 }
 
+class Date
+  def this_week
+    self - self.wday
+  end
+
+  def next_week
+    self + (7 - self.wday)
+  end
+
+  def next_wday(n)
+    n > self.wday ? self + (n - self.wday) : self.next_week.next_day(n)
+  end
+
+  def last_week
+    self.this_week - 7
+  end
+
+  def last_wday(n)
+    n < self.wday ? self - (self.wday - n) : self.last_week.next_day(n)
+  end
+end
+
 class PayrollController
   def initialize
     @start_date = nil
