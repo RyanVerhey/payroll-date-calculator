2ab74b0e663bcca59ea556f2f5e08d3a65e26f5f
RyanVerhey <ryan90v@gmail.com>
2014-06-19 22:17:35 -0500

    Moved the controller to its own file

--- run_script.rb
+++ run_script.rb
@@ -1,6 +1,7 @@
 require 'date'
 require 'yaml'
 require_relative '.date_extension'
+require_relative '.payroll_controller'
 require_relative '.payroll_calculator'
 
 controller = PayrollController.new
